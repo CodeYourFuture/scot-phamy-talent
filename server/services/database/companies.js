5f46c677d315422cce02d1760efd37cca0346bc1
Meriem <mmlys89@gmail.com>
2019-08-22 21:49:50 +0100

    Remove console log

--- server/services/database/companies.js
+++ server/services/database/companies.js
@@ -20,11 +20,9 @@ const registerCompany = ({ name, description, industry, user_id }) => {
       "INSERT INTO company_profile (name ,description , industry ,user_id) VALUES($1,$2,$3,$4)",
       [name, description, industry, user_id],
       (error, result) => {
-        console.log("Database", error, result);
         if (error) {
           reject(error);
         }
-        console.log(result);
         resolve(result.rows);
       }
     );
