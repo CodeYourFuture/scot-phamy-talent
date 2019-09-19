6b2db5897a882f995dcf35f855654510f693f7c0
Meriem <mmlys89@gmail.com>
2019-09-19 09:27:28 +0100

    Component for applicants list

---
+++ client/src/components/ApplicantsList.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import getAllApplicants from "../api/applicants";
+import ApplicantsCard from "./ApplicantsCard";
+
+export default class ApplicantsList extends React.Component {
+  state = {
+    applicantsList: []
+  };
+
+  componentDidMount() {
+    getAllApplicants().then(res => {
+      this.setState({
+        applicantsList: res
+      });
+    });
+  }
+  render() {
+    console.log(this.state.applicantsList);
+
+    return (
+      <div>
+        {this.state.applicantsList.map(applicant => (
+          <ApplicantsCard {...applicant} key={applicant.applicant_id} />
+        ))}
+      </div>
+    );
+  }
+}
